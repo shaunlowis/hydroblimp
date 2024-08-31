d8122214c9243c5537e644c193a09977632c98c1
slowis <johnny@dawnaerospace.com>
2024-08-31 13:37:01 +1200

    got sim working and actually useful plots

--- modeling/nesc_case7.py
+++ modeling/nesc_case7.py
@@ -1,34 +1,14 @@
-# Maybe this is useful?
-# https://www.youtube.com/watch?v=ERVrlzttLg4&t=1s
-
-# This paper is a great baseline for me:
-# https://www.ijmerr.com/uploadfile/2018/0822/20180822052610906.pdf
-
-
-# This report has some good content:
-# https://reports.nlr.nl/server/api/core/bitstreams/a55813d9-bd3a-462d-a220-f533be6312b7/content
-
-# This function seems most suited:
-# https://nasa.github.io/simupy-flight/api.html#simupy_flight.Planet.local_translational_trim_residual
-
-# Included example on trim condition for an aircraft:
-# simupy-flight/nesc_test_cases/nesc_case11.py
-
-# Explanation here:
-# https://nescacademy.nasa.gov/flightsim
-
-
 """
-===========================================================
-Case 4: Dropped sphere over non-rotating, spherical Earth
-===========================================================
+=======================================================
+Case 7: Sphere dropped through a steady wind field
+=======================================================
 
 ==============  ===============
-Verifies        Gravitation, integration
-Gravitation     :math:`1/R^2`
-Geodesy         Round fixed
+Verifies        Wind effects
+Gravitation     J2
+Geodesy         WGS-84 rotating
 Atmosphere      US 1976 STD
-Winds           still air
+Winds           steady 20 ft/s from due west
 Vehicle         Sphere with constant :math:`C_D`
 ==============  ===============
 """
@@ -41,13 +21,13 @@ from nesc_testcase_helper import plot_nesc_comparisons, int_opts, benchmark
 from nesc_testcase_helper import ft_per_m, kg_per_slug
 
 planet = simupy_flight.Planet(
-    gravity=simupy_flight.get_spherical_gravity(
-        simupy_flight.earth_spherical_gravity_constant
-    ),
-    winds=simupy_flight.get_constant_winds(),
+    gravity=simupy_flight.earth_J2_gravity,
+    winds=simupy_flight.get_constant_winds(wy=20 / ft_per_m),
     atmosphere=simupy_flight.atmosphere_1976,
     planetodetics=simupy_flight.Planetodetic(
-        a=20902255.199 / ft_per_m, omega_p=0.0, f=0.0
+        a=simupy_flight.earth_equitorial_radius,
+        omega_p=simupy_flight.earth_rotation_rate,
+        f=simupy_flight.earth_f,
     ),
 )
 
@@ -67,7 +47,6 @@ S_A = 0.1963495 / (ft_per_m**2)
 b_l = 1.0
 c_l = 1.0
 a_l = b_l
-
 vehicle = simupy_flight.Vehicle(
     base_aero_coeffs=simupy_flight.get_constant_aero(CD_b=0.1),
     m=m,
@@ -103,18 +82,15 @@ V_D_ic = 0.0
 psi_ic = 0.0 * np.pi / 180
 theta_ic = 0.0 * np.pi / 180
 phi_ic = 0.0 * np.pi / 180
-omega_X_ic = 10.0 * np.pi / 180
-omega_Y_ic = 20.0 * np.pi / 180
-omega_Z_ic = 30.0 * np.pi / 180
+omega_X_ic = 0.0 * np.pi / 180
+omega_Y_ic = 0.0 * np.pi / 180
+omega_Z_ic = 0.0 * np.pi / 180
 
 planet.initial_condition = planet.ic_from_planetodetic(
     long_ic, lat_ic, h_ic, V_N_ic, V_E_ic, V_D_ic, psi_ic, theta_ic, phi_ic
 )
 planet.initial_condition[-3:] = omega_X_ic, omega_Y_ic, omega_Z_ic
 
+# Run simulation for 30s
 with benchmark() as b:
     res = BD.simulate(30, integrator_options=int_opts)
-
-# %%
-
-plot_nesc_comparisons(res, "04")
--- modeling/setup.py
+++ modeling/setup.py
@@ -1,6 +1,24 @@
 # Instructed to use simupy.
 # Here we can configure our Vehicle object.
 
+#  Maybe this is useful?
+# https://www.youtube.com/watch?v=ERVrlzttLg4&t=1s
+
+# This paper is a great baseline for me:
+# https://www.ijmerr.com/uploadfile/2018/0822/20180822052610906.pdf
+
+# This report has some good content:
+# https://reports.nlr.nl/server/api/core/bitstreams/a55813d9-bd3a-462d-a220-f533be6312b7/content
+
+# This function seems most suited:
+# https://nasa.github.io/simupy-flight/api.html#simupy_flight.Planet.local_translational_trim_residual
+
+# Included example on trim condition for an aircraft:
+# simupy-flight/nesc_test_cases/nesc_case11.py
+
+# Explanation here:
+# https://nescacademy.nasa.gov/flightsim
+
 from simupy_flight import Vehicle, Planet
 
 # imports for planet setup, using print(help(Planet)), mirroring simupy-flight/nesc_test_cases/nesc_case01.py
