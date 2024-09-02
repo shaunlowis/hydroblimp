c91eed7d6907559f4856bc27b3bdd472e861854c
slowis <johnny@dawnaerospace.com>
2024-09-02 23:35:36 +1200

    more to do but getting there

--- modeling/trim_condition.py
+++ modeling/trim_condition.py
@@ -30,14 +30,25 @@ plt.rc("font", family="serif")
 plt.rc("text", usetex=True)
 plt.rc("font", **{"serif": ["Times New Roman"]})
 
+# planet = simupy_flight.Planet(
+#     gravity=simupy_flight.get_spherical_gravity(
+#         simupy_flight.earth_spherical_gravity_constant
+#     ),
+#     winds=simupy_flight.get_constant_winds(),
+#     atmosphere=simupy_flight.atmosphere_1976,
+#     planetodetics=simupy_flight.Planetodetic(
+#         a=20902255.199 / ft_per_m, omega_p=0.0, f=0.0
+#     ),
+# )
+
 planet = simupy_flight.Planet(
-    gravity=simupy_flight.get_spherical_gravity(
-        simupy_flight.earth_spherical_gravity_constant
-    ),
+    gravity=simupy_flight.earth_J2_gravity,
     winds=simupy_flight.get_constant_winds(),
     atmosphere=simupy_flight.atmosphere_1976,
     planetodetics=simupy_flight.Planetodetic(
-        a=20902255.199 / ft_per_m, omega_p=0.0, f=0.0
+        a=simupy_flight.earth_equitorial_radius,
+        omega_p=simupy_flight.earth_rotation_rate,
+        f=simupy_flight.earth_f,
     ),
 )
 
@@ -56,6 +67,7 @@ planet = simupy_flight.Planet(
 #                  0      2a^2     0
 #                  0       0    a^2+b^2
 #             )
+
 #     = 0.06836/5 * (
 #               2.749^2+0.4^2    0       0
 #                  0      2*0.4^2     0
@@ -98,14 +110,31 @@ g_ic = g_0 * ((R / (R + h_ic)) ** 2)
 # See report for justification
 g_avg = (g_ic + 9.81) / 2
 
-# Add in buoyancy force.
+# See report for derivations.
 vehicle = simupy_flight.Vehicle(
-    base_aero_coeffs=simupy_flight.get_constant_aero(CD_b=0.1),
+    base_aero_coeffs=simupy_flight.get_constant_aero(
+        CD_b=0.068386,
+        CL_b=0.6111,
+    ),
+    #     get_constant_force_moments(
+    #     FX=0.0,
+    #     FY=0.0,
+    #     FZ=0.0,
+    #     MX=0.0,
+    #     MY=0.0,
+    #     MZ=0.0,
+    # ):
+    #     """
+    #     Generate a force and moment callback that returns a constant and specified set of
+    #     forces and moments regardless of flight condition.
+    #     """
     input_force_moment=np.array(
         [
             0.0,  # FX
-            0.0,  # FY
-            -m * g_avg,  # FZ, buoyancy force
+            0.0,
+            # 2.0,  # FY, applied small thrust.
+            # -m * g_avg,  # FZ, buoyancy force
+            0.0,
             0.0,  # MX
             0.0,  # MY
             0.0,  # MZ
@@ -160,9 +189,55 @@ DEFAULT_INTEGRATOR_OPTIONS = {
     "name": "dopri5",
     "rtol": 1e-6,
     "atol": 1e-12,
-    "nsteps": 500,
+    "nsteps": 5000,
     "max_step": 0.0,
 }
 
 with benchmark() as b:
-    res_fine_gravity = BD.simulate(300, integrator_options=DEFAULT_INTEGRATOR_OPTIONS)
+    res = BD.simulate(20, integrator_options=DEFAULT_INTEGRATOR_OPTIONS)
+
+# Output is ordered by simulation setup, so Planet dims then Vehicle dims
+all_cols = planet.output_column_names_latex + vehicle.output_column_names_latex
+
+fig, ax = plt.subplots(3, 2, figsize=(8, 6), constrained_layout=True, sharex=True)
+
+# LEFT SIDE PLOTS:
+# Euler angles:
+ax[0][0].set_title("Euler angles")
+for i, euler_angle in enumerate(all_cols[16:19]):
+    ax[0][0].plot(res.t, res.y[:, i], label=f"{euler_angle} [NED]")
+
+# Translational acceleration:
+ax[1][0].set_title("True air speed")
+ax[1][0].plot(res.t, res.y[:, 22], label="$V_T$")
+
+# Angular acceleration:
+ax[2][0].set_title("Altitude")
+ax[2][0].plot(res.t, res.y[:, 15], label=all_cols[15])
+
+
+# RIGHT SIDE PLOTS:
+# Transational acceleration of vehicle
+ax[0][1].set_title("Translational acceleration of vehicle")
+for l, accel_out in enumerate(all_cols[34:37]):
+    ax[0][1].plot(res.t, res.y[:, l], label=f"{accel_out} [FRD]")
+    print(f"{accel_out} = {res.y[:, l].mean():.3g}")
+
+ax[1][1].set_title("Angular acceleration of vehicle")
+for m, angles_out in enumerate(all_cols[37:]):
+    ax[1][1].plot(res.t, res.y[:, m], label=f"{angles_out}")
+    print(f"{angles_out} = {res.y[:, m].mean():.3g}")
+
+ax[2][1].set_title("Momenta of vehicle")
+for n, momenta in enumerate(all_cols[:3]):
+    ax[2][1].plot(res.t, res.y[:, n], label=f"{momenta}")
+    print(f"{momenta} = {res.y[:, n].mean():.3g}")
+
+for sub_ax in fig.axes:
+    sub_ax.grid(which="major", linestyle="--", linewidth=0.5)
+    sub_ax.legend()
+
+fig.supxlabel("Simulation time [s]")
+fig.suptitle("Vehicle in trimmed flight, 2N thrust in +y")
+
+plt.savefig("modeling/report_plots/trimmed_flight.pdf")
